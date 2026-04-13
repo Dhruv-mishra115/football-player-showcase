70aca0c69cda28939d15e005d88e816345b3ac5c
Dhruv-INF <dhruvmishra.19611@gmail.com>
2026-04-13 16:16:39 +0530

    Added UI upgrade, animations, and authentication system

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request , redirect, session 
+from flask import session, redirect
 
 app = Flask(__name__)
+app.secret_key = "secret123"
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
+)
+""")
+
+conn.commit()
+conn.close()
 
 @app.route('/')
 def home():
@@ -9,6 +25,11 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+
+    # 🔒 ADD THIS (just 2 lines)
+    if 'user' not in session:
+        return redirect('/login')
+
     if request.method == 'POST':
         name = request.form['name']
         age = request.form['age']
@@ -72,6 +93,53 @@ def player(id):
     conn.close()
 
     return render_template("player.html", player=player)
+
+from flask import request, redirect, render_template
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+
+        conn.commit()
+        conn.close()
+
+        return redirect('/login')
+
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        user = cursor.fetchone()
+
+        conn.close()
+
+        if user:
+            session['user'] = username
+            return redirect('/')
+        else:
+            return "Invalid Credentials"
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
 
 if __name__ == '__main__':
     init_db()
